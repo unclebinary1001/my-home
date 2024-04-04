a2cb6d2377bc4e726720508ff5e175678dd020b5
Mahlangu <nzundamahlangu@gmail.com>
2024-04-04 05:15:31 -0500

    add about me to container

--- my-portfolio/components/container.tsx
+++ my-portfolio/components/container.tsx
@@ -2,12 +2,14 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import { Header } from "./header";
 import Navbar from "./navbar";
+import { About } from "./about";
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
     return (
         <Box maxW="1200px" mx="auto" px={8}>
             <Navbar />
             <Header />
+            <About />
             {children}
         </Box>
     );
