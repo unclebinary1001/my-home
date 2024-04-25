558d3dac72a7274e75eab4e79a9f3e3dd0bbd9be
Mahlangu <nzundamahlangu@gmail.com>
2024-04-25 03:32:01 -0500

    add sticky navbar to container

--- my-portfolio/components/container.tsx
+++ my-portfolio/components/container.tsx
@@ -7,12 +7,16 @@ import { Projects } from "./projects";
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
     return (
-        <Box maxW="1200px" mx="auto" px={8}>
-            <Navbar />
+        <>
+        <Box maxW="1200px" mx={"auto"} zIndex={1} position={"sticky"} top={0} px={16} bg={"inherit"}>
+        <Navbar />
+        </Box>
+        <Box maxW="1200px" mx="auto" px={16}>
             <Header />
             <About />
             <Projects />
             {children}
         </Box>
+        </>
     );
 };
