08867642a5f7d1a8afa991bd9c5d24c998bed347
unclebinary1001 <nzundamahlangu@gmail.com>
2025-03-07 23:00:46 -0600

    fix: add google site verification

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
       <head>
         <title>Mahlangu | Portfolio</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name="google-site-verification" content="aZOBDEpmO5mlrgII26w8bAVgu7UDnFZaG6X2vLYdJkQ" />
         <link rel='icon' href='/favicon-32x32.png' />
       </head>
       <body style={{backgroundColor: `#F8F8FB`}}>
